200eede530c90e98e6db14108b518b592693e02d
IsaiasPC <isamed92@gmail.com>
2019-03-16 21:40:59 -0300

    alert controller: add a list to the service

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Lista } from 'models/lista.model';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,11 +12,42 @@ import { Router } from '@angular/router';
 export class Tab1Page {
 
   constructor(public deseosService: DeseosService,
-              private router: Router) {
+              private router: Router,
+              private alertCtrl: AlertController) {
   }
 
 
-  agregarLista() {
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  async agregarLista() {
+    const alert = await this.alertCtrl.create({
+      header: 'Nueva lista',
+      inputs: [{
+        name: 'titulo',
+        type: 'text',
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            console.log(data);
+            if ( data.titulo.length === 0) {
+              return;
+            }
+            // Tengo que crear la lista
+            this.deseosService.crearLista(data.titulo);
+
+          }
+        }
+      ]
+    });
+     alert.present();
+    // this.router.navigateByUrl('/tabs/tab1/agregar');
   }
 }
